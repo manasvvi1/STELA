a4ae65c07583d3852122398cfc22754b378b452e
FlutLab.io <>
2022-06-29 15:22:51 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.orange,
       ),
-      home: MyHomePage(title: 'Flutter Test 4'),
+      home: MyHomePage(title: 'Flutter Demo'),
     );
   }
 }
