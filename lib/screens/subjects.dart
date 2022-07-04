1b823e636a570bfdb805692386d9eab8d833fe87
Manasvvi <manasvvi.aggarwal@gmail.com>
2022-07-04 23:51:06 +0530

    test analysis and subjects page added

--- lib/screens/subjects.dart
+++ lib/screens/subjects.dart
@@ -10,18 +10,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+        home: Scaffold(
           backgroundColor: Color(0xffEDC7C7),
-          body: Container(
-              height: 105,
-              margin: EdgeInsets.symmetric(vertical: 100, horizontal: 10),
-              alignment: Alignment.center,
-              padding: EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  Text('Test Analysis', style: TextStyle(fontSize: 25, fontFamily: 'Mandorlato', fontWeight: FontWeight.bold)),
-                ],
-              )
+          body: SafeArea(
+            child: Container(
+                height: 105,
+                margin: EdgeInsets.symmetric(vertical: 100, horizontal: 10),
+                alignment: Alignment.center,
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Color(0xff93B5C6),
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1,
+                    style: BorderStyle.solid,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  boxShadow: [BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),],
+                ),
+                child: Column(
+                  children: [
+                    Text('Artificial Intelligence', style: TextStyle(fontSize: 25, fontFamily: 'Mandorlato')),
+                    Text('M. Tech.', style: TextStyle(fontSize: 20, fontFamily: 'Mandorlato')),
+                    Text('>>>', style: TextStyle(fontSize: 25, fontFamily: 'Mandorlato'))
+                  ],
+                )
+            ),
           ),
           bottomNavigationBar: Container(
             padding: EdgeInsets.all(7),
@@ -46,8 +67,8 @@ class MyApp extends StatelessWidget {
                 ),
               ],
             ),
-          )
-      )
+          ),
+        )
     );
   }
 }
