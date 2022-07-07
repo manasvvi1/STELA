bf26dddc950229751bd201ea35c24c155e730ef1
FlutLab.io <>
2022-07-07 18:24:57 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class _MyAppState extends State<MyApp> {
                           },
                         ),
                         FlatButton(
-                          padding: EdgeInsets.all(0),
+                          padding: EdgeInsets.all(10),
                           child: Container(
                               width: double.infinity,
                               padding: EdgeInsets.symmetric(vertical: 5),
