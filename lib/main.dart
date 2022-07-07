8f67daf5e3b82a853024b2aaf68081a2b4516ffe
FlutLab.io <>
2022-07-07 18:19:10 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: primPink,
+          backgroundColor: primIceBlue,
           body: SingleChildScrollView(
             child: Container(
               margin: EdgeInsets.symmetric(vertical: 100, horizontal: 10),
