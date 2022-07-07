9693134862dfa8fa6db7f5441e30b5244eae20d5
Manasvvi <manasvvi.aggarwal@gmail.com>
2022-07-07 11:39:18 +0530

    experiments added

--- lib/screens/experiment.dart
+++ lib/screens/experiment.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:stela_app/constants/colors.dart';
 import 'package:stela_app/constants/experiments.dart';
 
-var num = 0;
+var num = 1;
 
 void main() {
   runApp(MyApp());
