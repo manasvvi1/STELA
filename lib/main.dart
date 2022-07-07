a633611634ee4e7ac43d8f7bffa389df54aed5b7
FlutLab.io <>
2022-07-07 19:54:28 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -17,76 +17,22 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      backgroundColor: Colors.transparent,
-      //backgroundImage: AssetImage("asssets/images/splash_screen.png"),
-      body: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.symmetric(vertical: 100, horizontal: 10),
-          alignment: Alignment.topCenter,
-          padding: EdgeInsets.all(60),
-          child: Column(
-            children: [
-              Container(padding: EdgeInsets.all(10), child: Text('STELA', style: TextStyle(color: Colors.white, fontSize: 35, fontFamily: 'Mandorlato-ExtraBold', fontWeight: FontWeight.bold))),
-              Container(decoration: new BoxDecoration(
-              image: DecorationImage(
-              image: new AssetImage(
-                  'assets/images/splash_screen.png'),
-              fit: BoxFit.fill,
-              ),))
-              Container(
-                child: Column(
-                  children: [
-                    FlatButton(
-                      padding: EdgeInsets.all(50),
-                      child: Container(
-                          width: double.infinity,
-                          padding: EdgeInsets.symmetric(vertical: 5),
-                          decoration: BoxDecoration(
-                            color: primPink,
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(width: 2.0, color: primLightPink),
-                          ),
-                          child: Text(
-                            'LOG IN',
-                            style: TextStyle(fontSize: 20, fontFamily: 'Mandorlato-Bold', fontWeight: FontWeight.bold),
-                            textAlign: TextAlign.center,
-                          )),
-                      onPressed: () {
-                        setState(() {
-                          expNum = 0;
-                        });
-                        print(expNum);
-                      },
-                    ),
-                    FlatButton(
-                      padding: EdgeInsets.all(50),
-                      child: Container(
-                          width: double.infinity,
-                          padding: EdgeInsets.symmetric(vertical: 5),
-                          decoration: BoxDecoration(
-                            color: primLightPink,
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(width: 2.0, color: primLightPink),
-                          ),
-                          child: Text(
-                            'SIGN UP',
-                            style: TextStyle(fontSize: 20, fontFamily: 'Mandorlato-Bold', fontWeight: FontWeight.bold),
-                            textAlign: TextAlign.center,
-                          )),
-                      onPressed: () {
-                        setState(() {
-                          expNum = 1;
-                        });
-                        print(expNum);
-                      },
-                    ),
-                  ],
+            backgroundColor: Colors.transparent,
+            //backgroundImage: AssetImage("asssets/images/splash_screen.png"),
+            body: SingleChildScrollView(
+                child: new Column(children: [
+              new Container(
+                child: new Text(
+                  'STELA',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 35, fontFamily: 'Mandorlato-ExtraBold', fontWeight: FontWeight.bold, color: Colors.white),
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    ));
+              new Container(
+                height: 200.0,
+                width: 200.0,
+                decoration: new BoxDecoration(image: DecorationImage(image: new AssetImage('assets/images/home.png'), fit: BoxFit.fill), shape: BoxShape.circle),
+              ),
+            ]))));
   }
 }
