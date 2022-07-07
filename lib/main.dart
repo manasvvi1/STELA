21b86f8ab8beaee7e5018ddcf25ac38a348a7510
FlutLab.io <>
2022-07-07 19:58:23 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -16,23 +16,93 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.transparent,
-            //backgroundImage: AssetImage("asssets/images/splash_screen.png"),
-            body: SingleChildScrollView(
-                child: new Column(children: [
-              new Container(
-                child: new Text(
-                  'STELA',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 35, fontFamily: 'Mandorlato-ExtraBold', fontWeight: FontWeight.bold, color: Colors.white),
+      home: Scaffold(
+          backgroundColor: primPink,
+          body: SingleChildScrollView(
+            child: Container(
+              margin: EdgeInsets.symmetric(vertical: 100, horizontal: 10),
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Container(padding: EdgeInsets.all(10), child: Text('STELA', style: TextStyle(color: Colors.white30, fontSize: 35, fontFamily: 'Mandorlato-ExtraBold', fontWeight: FontWeight.bold))),
+                  Container(
+                    child: Column(
+                      children: [
+                        FlatButton(
+                          padding: EdgeInsets.all(0),
+                          child: Container(
+                              width: double.infinity,
+                              padding: EdgeInsets.symmetric(vertical: 5),
+                              decoration: BoxDecoration(
+                                color: primIceBlue,
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(width: 2.0, color: primBlue),
+                              ),
+                              child: Text(
+                                'LOG IN',
+                                style: TextStyle(fontSize: 20, fontFamily: 'Mandorlato-Bold', fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              )),
+                          onPressed: () {
+                            setState(() {
+                              expNum = 0;
+                            });
+                            print(expNum);
+                          },
+                        ),
+                        FlatButton(
+                          padding: EdgeInsets.all(0),
+                          child: Container(
+                              width: double.infinity,
+                              padding: EdgeInsets.symmetric(vertical: 5),
+                              decoration: BoxDecoration(
+                                color: primIceBlue,
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(width: 2.0, color: primBlue),
+                              ),
+                              child: Text(
+                                'SIGN UP',
+                                style: TextStyle(fontSize: 20, fontFamily: 'Mandorlato-Bold', fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              )),
+                          onPressed: () {
+                            setState(() {
+                              expNum = 1;
+                            });
+                            print(expNum);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          bottomNavigationBar: Container(
+            padding: EdgeInsets.all(7),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const <Widget>[
+                Icon(
+                  Icons.home,
+                  color: Colors.black,
+                  size: 40,
                 ),
-              ),
-              new Container(
-                height: 200.0,
-                width: 200.0,
-                decoration: new BoxDecoration(image: DecorationImage(image: new AssetImage('assets/images/home.png'), fit: BoxFit.fill), shape: BoxShape.circle),
-              ),
-            ]))));
+                Icon(
+                  Icons.saved_search_rounded,
+                  color: Colors.black,
+                  size: 40,
+                ),
+                Icon(
+                  Icons.account_circle,
+                  color: Colors.black,
+                  size: 40,
+                ),
+              ],
+            ),
+          )),
+    );
   }
 }
